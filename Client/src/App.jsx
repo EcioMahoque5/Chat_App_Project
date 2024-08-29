d8eddec090b825625eccb66ad2ce9b1fa57eca67
Ecio Mahoque <emahoque@onfonmobile.co.mz>
2024-08-29 10:22:27 +0200

    Installation of some libraries

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ChatSection from './components/ChatSection';
 import LoginPage from './components/LoginPage';
+import ForgotPasswordPage from './components/ForgotPasswordPage';
 
 const App = () => {
   const [userName, setUserName] = useState('');
 
   return (
     <Router>
+      <ToastContainer newestOnTop={false}/>
       <Routes>
         <Route path="/" element={<LoginPage onLogin={(name) => setUserName(name)} />} />
         <Route path="/chat" element={<ChatSection userName={userName} />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       </Routes>
     </Router>
   );
--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './public',  // Ensure this path matches your Flask static folder
+    emptyOutDir: true,
+  },
 })
